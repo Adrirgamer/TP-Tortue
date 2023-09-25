194de107ab9db7b21c57adf3cea110f052aae756
Adrien LABASTIRE <a.labastire@workstation5.nsi.lan>
2023-09-25 17:03:00 +0200

    Copier-coller depuis le cours, début du TP

---
+++ tortue.py
@@ -0,0 +1,12 @@
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+t.speed(0) # maximum speed
+
+t.forward(100)
+t.left(90)
+t.forward(50)
+t.right(90)
+t.forward(100)
+
+turtle.exitonclick()
