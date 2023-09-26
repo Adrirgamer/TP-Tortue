2730e5937dcea0b77c864cc7b2c02b840ef7f0d9
Adrien LABASTIRE <a.labastire@workstation5.nsi.lan>
2023-09-26 14:22:56 +0200

    v2

--- tortue.py
+++ tortue.py
@@ -1,6 +1,28 @@
 import turtle
 
 t = turtle.Turtle()
+        
+def polygone(longueur, nb_cotes, soustrait):
+    t.hideturtle()
+    for _ in range(nb_cotes):
+        longueur = longueur - int(soustrait)
+        t.forward(int(longueur))
+        t.left(360/nb_cotes)
+
+def spirograph(longueurinit, rotation, couleur, cran):
+    """longueurinit = longueur du coté inital,
+       rotation = degré de rotation de chaque carré par rapport au précédent
+       couleur = couleur du trait
+       cran = différence de longueur a chaque coté tracé
+    """
+   # t.hideturtle()
+    for _ in range(360/int(rotation)):
+        t.color(couleur)
+        rotation = rotation + 360/int(rotation)
+        for _ in range(4):
+            longueurinit=longueurinit + int(cran)
+            t.forward(longueurinit)
+            t.right(rotation)
 
 def equ(longueur):
     t.hideturtle()
@@ -13,13 +35,14 @@ def carre(longueur):
     for _ in range(4):
         t.forward(longueur)
         t.left(90)
+
+#carre(float(input("longueur:")))
+
+#equ(float(input("longueur:")))
         
-def polygone(longueur, nb_cotes):
-    t.hideturtle()
-    for _ in range(nb_cotes):
-        t.forward(longueur)
-        t.left(360/nb_cotes)
-        
-      
-polygone(int(input("longueur:")), int(input("nb_cotes:")))
+spirograph(int(input("longueur initiale : ")), int(input("rotation : ")), str(input("couleur : ")) , int(input("cran : ")))
+
+#polygone(float(input("longueur : ")), int(input("nb_cotes : ")), int(input("soustrait : ")))
+
+
 turtle.exitonclick()
