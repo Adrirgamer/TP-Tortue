73a0812b733779bd3862af44974a223f795fed1c
Adrien LABASTIRE <a.labastire@workstation5.nsi.lan>
2023-09-26 10:59:39 +0200

    version 1

--- tortue.py
+++ tortue.py
@@ -1,10 +1,18 @@
 import turtle
+
+t = turtle.Turtle()
+
 def equ(longueur):
-    t = turtle.Turtle()
     t.hideturtle()
     for _ in range(3):
         t.forward(longueur)
         t.left(120)
-        t.hideturtle()
-equ(float(input("longueur:")))
+
+def carre(longueur):
+    t.hideturtle()
+    for _ in range(4):
+        t.forward(longueur)
+        t.left(90)
+      
+carre(float(input("longueur:")))
 turtle.exitonclick()
